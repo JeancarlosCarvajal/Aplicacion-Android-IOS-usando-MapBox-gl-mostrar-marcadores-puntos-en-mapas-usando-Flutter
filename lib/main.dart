6b6343c2ae0e77529198e0eb431559084f4fd318
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-07-20 18:41:48 -0400

    Mapa viendo coordenadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mapbox_mapas/src/views/flull_screen_map.dart';
 
-void main() => runApp(MyApp());
+Future main() async {
+
+  // To load the .env file contents into dotenv.
+  // NOTE: fileName defaults to .env and can be omitted in this case.
+  // Ensure that the filename corresponds to the path in step 1 and 2.
+  await dotenv.load(fileName: ".env");
+  
+  runApp(const MyApp());
+
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
--- lib/src/views/flull_screen_map.dart
+++ lib/src/views/flull_screen_map.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
-import '../api_key/apis_key.dart';
 
 class FullScreenMap extends StatefulWidget {
    
@@ -11,17 +11,20 @@ class FullScreenMap extends StatefulWidget {
 }
 
 class _FullScreenMapState extends State<FullScreenMap> {
-  @override
-  Widget build(BuildContext context) {
 
-    // return Container();
     
     MapboxMapController? mapController;
-    var isLight = true;
 
     _onMapCreated(MapboxMapController controller) {
       mapController = controller;
     }
+
+    final LatLng center = LatLng(10.1923226, -64.6841399);
+
+    // estilo creado personalizado en https://studio.mapbox.com/.. lo copie desde Style URL
+    var selectedStyle = 'mapbox://styles/jcc9638/cl5twvcjk001w15payzt6ao7v';
+    final oscuroStyle = 'mapbox://styles/jcc9638/cl5twvcjk001w15payzt6ao7v';
+    final streetStyle = 'mapbox://styles/jcc9638/cl5twyqdy000t14tdw50rj51f';
 
     _onStyleLoadedCallback() {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -30,24 +33,40 @@ class _FullScreenMapState extends State<FullScreenMap> {
         duration: const Duration(seconds: 1),
       ));
     }
- 
-    // const accessToken = String.fromEnvironment("MAPBOX_DOWNLOADS_TOKEN");
 
-    // print('Token: $accessToken');
-
-    return Scaffold(
+    final apiMapBox = dotenv.env['MAPBOX_TOKEN_STYLES']; // MAPBOX_TOKEN_STYLES   MAPBOX_TOKEN
+    var isLight = true;
+  @override
+  Widget build(BuildContext context) { 
+    return Scaffold( 
+      // appBar: AppBar(
+      //   title: Center(child: Text('Hola Mundo')),
+      // ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        child: const Icon(Icons.swap_horiz),
+        onPressed: () {
+          selectedStyle == oscuroStyle
+            ? selectedStyle = streetStyle
+            : selectedStyle = oscuroStyle;
+          setState(() { 
+            print(selectedStyle);
+          });
+        }
+      ), 
       body: MapboxMap(
+        styleString: selectedStyle, 
         // styleString: isLight ? MapboxStyles.LIGHT : MapboxStyles.DARK,
         // TODO sin este token accessToken la aplicacion REBIENTA OJO
-        accessToken: ApisKey.apiMapBox,
+        accessToken: apiMapBox,
         onMapCreated: _onMapCreated,
         initialCameraPosition: 
-          const CameraPosition(
-            target: LatLng(10.1923226, -64.6841399),
+          CameraPosition(
+            target: center,
             zoom: 13
           ),
         onStyleLoadedCallback: _onStyleLoadedCallback,
       ),
     );
   }
-}
+} 
